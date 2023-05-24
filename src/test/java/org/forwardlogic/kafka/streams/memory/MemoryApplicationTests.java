7b7ba353ab6f90e0bea6ae1377ebeb5310ac286a
rbrunet <rbrunet@forwardlogictech.com>
2023-05-24 14:24:13 -0400

    First commit

---
+++ src/test/java/org/forwardlogic/kafka/streams/memory/MemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.forwardlogic.kafka.streams.memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
